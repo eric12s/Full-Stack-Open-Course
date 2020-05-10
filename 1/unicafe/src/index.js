bbbc2855e8398a71ad0c72b28d2d41c35f14c53b
eric12s <ericsabag@gmail.com>
2020-05-10 03:19:52 +0300

    finish unicafe

--- 1/unicafe/src/index.js
+++ 1/unicafe/src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Statistic = ({text, value}) => (
+  <tr><td>{text}</td><td>{value}</td></tr>
+)
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad
+  const avg = (props.good - props.bad) / total || 0
+  const perc = ((props.good / total) * 100)+'%' || 100+'%'
+
+  if(props.good === 0 && props.neutral === 0 && props.bad === 0)
+    return <p>No feedback given</p>
+
+  return(
+    <div>
+      <table>
+        <tbody>
+          <Statistic text='good' value={props.good}/>
+          <Statistic text='neutral' value={props.neutral}/>
+          <Statistic text='bad' value={props.bad}/>
+          <Statistic text='all' value={total}/>
+          <Statistic text='average' value={avg}/>
+          <Statistic text='positive' value={perc}/>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const Button = (props) => (
+  <button onClick={props.handle}>{props.text}</button>
+)
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handle={() => {setGood(good + 1)}} text='good'/>
+      <Button handle={() => {setNeutral(neutral + 1)}} text='neutral'/>
+      <Button handle={() => {setBad(bad + 1)}} text='bad'/>
+      <h2>statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
